c7ce3391ef4d99cd5ad6a768537476b8b94ae14a
aymanaboghonim <aymanaboghonim@gmail.com>
2025-10-21 07:31:01 +0300

    Integrate About section into App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,8 @@ function App() {
       </header>
       
       <main className="main">
-        <Hero />
+  <Hero />
+  <About />
 
         <section className="features">
           <div className="container">
